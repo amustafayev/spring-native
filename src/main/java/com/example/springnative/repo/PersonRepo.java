05c74af32958ae1630710a00596dab53d8d15ecf
amustafayev <afgan.mustafayev@pashabank.az>
2023-01-25 14:33:10 +0400

    Refactor code

--- src/main/java/com/example/springnative/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springnative.repo;
-
-import com.example.springnative.model.PersonEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepo extends CrudRepository<PersonEntity,Long> {
-}
